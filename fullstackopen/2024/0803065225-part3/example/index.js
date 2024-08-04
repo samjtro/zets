66289a756883b674da51fc94fdafb9c5a7d0f02c
samjtro <hi@samjtro.com>
2024-08-04 11:40:36 -0500

    working on - fullstackopen/part3

--- fullstackopen/2024/0803065225-part3/example/index.js
+++ fullstackopen/2024/0803065225-part3/example/index.js
@@ -1,10 +1,33 @@
-const http = require('http')
-
+const express = require('express')
+const exp = express()
+/* const http = require('http')
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
   response.end('Hello World')
 })
+let notes = {}
+const app = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'application/json'})
+  response.end(JSON.stringify(notes))
+})*/
+
+let notes = [
+  {
+    id: 1,
+    content: 'hello, world'
+  }
+]
+
+exp.get('/', (req, resp) => {
+  resp.send('<h1>hello, world</h1>')
+})
+
+exp.get('/api/notes', (req, resp) => {
+  resp.json(notes)
+})
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+
+exp.listen(PORT, () => {
+  console.log(`[log] running on port ${PORT}`)
+})
